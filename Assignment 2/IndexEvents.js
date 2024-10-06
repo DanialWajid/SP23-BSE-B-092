4c1a4cecf705542ebbf6e719962f2070a426faff
DanialWajid <danialwajid112@gmail.com>
2024-10-06 18:35:02 +0500

    womp womp

---
+++ Assignment 2/IndexEvents.js
@@ -0,0 +1,42 @@
+window.onload=()=>{
+    let btnAd=document.getElementById("AdCancel");
+    btnAd.onclick=(event)=>{
+        event.preventDefault();
+        let ad=document.getElementById("ad");
+       ad.remove("add");
+    } 
+let btn1=document.getElementById("btn1");
+btn1.onclick=(event)=>{
+    event.preventDefault();
+    let top=document.getElementById("top");
+    top.remove("remove");
+}
+
+let heart = document.getElementById("heart");
+let person = document.getElementById("user");
+let bag = document.getElementById("bag");
+
+let array = [];
+array.push(heart);
+array.push(person);
+array.push(bag);
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+
+    element.onmouseenter = (event) => {
+        element.classList.remove("bg-white");
+        element.classList.add("bg-secondary");
+    };
+    
+    element.onmouseleave = (event) => {
+        element.classList.remove("bg-secondary");
+        element.classList.add("bg-white");
+    };
+
+
+}
+
+
+}
+
