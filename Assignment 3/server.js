2d0f8a04ccf02489dfd306997fc93533d996b969
DanialWajid <danialwajid112@gmail.com>
2024-11-06 17:01:09 +0500

    go

---
+++ Assignment 3/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+let server = express();
+server.set("view engine", "ejs");
+server.use(express.static("public"));
+
+server.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.get("/checkout", (req, res) => {
+  res.render("checkout");
+});
+server.get("/login", (req, res) => {
+  res.render("login");
+});
+server.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+server.listen(5001, () => {
+  console.log("server started at localhost : 5000");
+});
