e6d7a4a2a8d071a2b4493116da4facd8efc3cc81
DanialWajid <danialwajid112@gmail.com>
2024-11-14 21:31:42 +0500

    upload

--- Lab Task 3/public/js/script.js
+++ Lab Task 3/public/js/script.js
@@ -2,19 +2,20 @@ window.onload = show;
 
 function show() {
   $("#proj1").click(function () {
-    loadDescription1("/text/project1.txt").hide();
+    loadDescription1("/text/project1.txt");
   });
   $("#proj2").click(function () {
-    loadDescription2("/text/project2.txt").hide();
+    loadDescription2("/text/project2.txt");
   });
   $("#proj3").click(function () {
-    loadDescription3("/text/project3.txt").hide();
+    loadDescription3("/text/project3.txt");
   });
 }
 
 function loadDescription1(fileName) {
   $.get(fileName, function (data) {
     $("#description1").html(data).show();
+    $("#btn1").show();
   });
   if (!$("#cOne").find(".hide-btn").length) {
     $("#cOne").append("<button id=btn1 class='hide-btn'>Hide</button>");
@@ -30,6 +31,7 @@ function loadDescription1(fileName) {
 function loadDescription2(fileName) {
   $.get(fileName, function (data) {
     $("#description2").html(data).show();
+    $("#btn2").show();
   });
 
   if (!$("#cTwo").find(".hide-btn").length) {
@@ -47,6 +49,7 @@ function loadDescription2(fileName) {
 function loadDescription3(fileName) {
   $.get(fileName, function (data) {
     $("#description3").html(data).show();
+    $("#btn3").show();
   });
   if (!$("#cThree").find(".hide-btn").length) {
     $("#cThree").append("<button  id=btn3 class='hide-btn'>Hide</button>");
